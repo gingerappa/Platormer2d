33ddbf942ad383ffeeec79f8af97ae82b30f9b9c
gingerappa <t.swinkels@sintlucasedu.nl>
2022-12-16 22:34:31 +0100

    added second player

--- Assets/scripts/bullet.cs
+++ Assets/scripts/bullet.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bullet : MonoBehaviour
 {
     public float speed = 15f;
     public float lifeTime = 3f;
     public float dirx = 1f;
-    
+    public GameObject origin;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        try
+        {
+            collision.gameObject.GetComponent<movement>().health -= 1;
+            origin.GetComponent<movement>().score += 1;
+            origin.GetComponent<movement>().scoreText.GetComponent<Text>().text = origin.GetComponent<movement>().score.ToString();
+        }
+        finally
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void Start()
     {
         Destroy(gameObject, lifeTime);
--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,27 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movement : MonoBehaviour
 {
     public float speed;
     public GameObject bullet;
+    public float reloadSpeed;
+    public GameObject scoreText;
+    public int health;
+    public int score;
 
     private float facingDirX = 1f;
-
-    void Update()
+    private float Diry = 0f;
+    private float Dirx = 0f;
+    private float reloadTime = 0f;
+    private Vector3 spawnPos;
+    private void Start()
     {
-        float dirx = Input.GetAxisRaw("Horizontal");
-        transform.Translate(transform.right * dirx * speed * Time.deltaTime);
-        if(dirx != 0)
-        {
-            facingDirX = dirx;
-        }
+        spawnPos = transform.position;
+    }
 
-        if (Input.GetButtonDown("Fire1"))
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "floor")
         {
-            GameObject Bullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            Bullet.GetComponent<bullet>().dirx = facingDirX;
+            Diry = 0f;
         }
     }
+    void Update()
+    {
+        if (health <= 0)
+        {
+            transform.position = spawnPos;
+            health = 1;
+        }
+        if (transform.position.y <= -11)
+        {
+            health -= 1;
+        }
+        if (name == "player 1")
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                Dirx = 1f;
+                facingDirX = 1f;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                Dirx = 1f;
+                facingDirX = -1f;
+            }
+            else
+            {
+                Dirx = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Diry = 1f;
+            }
+            if (Input.GetKeyDown(KeyCode.E) && reloadTime <= 0)
+            {
+                GameObject Bullet = Instantiate(bullet, new Vector3(transform.position.x + (facingDirX * 2), transform.position.y, transform.position.z), Quaternion.identity);
+                Bullet.GetComponent<bullet>().dirx = facingDirX;
+                Bullet.GetComponent<bullet>().origin = gameObject;
+                reloadTime = reloadSpeed;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                Dirx = 1f;
+                facingDirX = 1f;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                Dirx = 1f;
+                facingDirX = -1f;
+            }
+            else
+            {
+                Dirx = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Diry = 1f;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad6) && reloadTime <= 0)
+            {
+                GameObject Bullet = Instantiate(bullet, new Vector3(transform.position.x+(facingDirX*2), transform.position.y, transform.position.z), Quaternion.identity);
+                Bullet.GetComponent<bullet>().dirx = facingDirX;
+                Bullet.GetComponent<bullet>().origin = gameObject;
+                reloadTime = reloadSpeed;
+            }
+        }
+        reloadTime -= Time.deltaTime;
+        transform.Translate(speed * Dirx * Time.deltaTime, speed * Diry * Time.deltaTime, 0);
+    }
 }
+
